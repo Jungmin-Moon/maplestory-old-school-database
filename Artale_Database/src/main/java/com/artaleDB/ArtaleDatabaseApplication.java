2df68a309a34295bb2744299b88b7d47a5f8cafb
Jungmin Moon <jmmoon96@gmail.com>
2024-11-27 21:04:51 -0500

    Starting commit

---
+++ Artale_Database/src/main/java/com/artaleDB/ArtaleDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.artaleDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtaleDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtaleDatabaseApplication.class, args);
+	}
+
+}
---
+++ Artale_Database/src/test/java/com/artaleDB/ArtaleDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artaleDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtaleDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
