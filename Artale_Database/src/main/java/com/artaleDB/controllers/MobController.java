52decff92452705089b0ee2b8b57a2b7891ab764
Jungmin Moon <jmmoon96@gmail.com>
2024-12-21 04:46:11 -0500

    Added then deleted some things after testing.

--- Artale_Database/src/main/java/com/artaleDB/controllers/MobController.java
+++ Artale_Database/src/main/java/com/artaleDB/controllers/MobController.java
@@ -1,6 +1,5 @@
 package com.artaleDB.controllers;
 
-import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.artaleDB.entities.Mob;
 import com.artaleDB.services.MobService;
