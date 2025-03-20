e54fe00a1bbe139f94716cd813a2d51d6da0efe3
Jungmin Moon <jmmoon96@gmail.com>
2025-03-20 03:13:39 -0400

    Re-factoring project to make it better.

--- Artale_Database/src/main/java/com/artaleDB/repositories/MobRepository.java
+++ Artale_Database/src/main/java/com/artaleDB/repositories/MobRepository.java
@@ -11,36 +11,5 @@ import com.artaleDB.entities.Mob;
 @Repository
 public interface MobRepository extends JpaRepository<Mob, Long>{
 	
-	@Query("SELECT m FROM Mob m WHERE m.mobName LIKE %:name%")
-	public List<Mob> getByName(String name);
 	
-	@Query("SELECT m FROM Mob m WHERE m.mobName = :name")
-	public Mob getByExactName(String name);
-	
-	@Query("SELECT m FROM Mob m WHERE m.mobLocation LIKE %:location% or m.mobLocationTwo LIKE %:location%")
-	public List<Mob> getByLocations(String location);
-	
-	@Query("SELECT m FROM Mob m ORDER BY m.mobNeededAccuracy DESC")
-	public List<Mob> getByAccuracyDesc();
-	
-	@Query("SELECT m FROM Mob m ORDER BY m.mobNeededAccuracy ASC")
-	public List<Mob> getByAccuracyAsc();
-	
-	@Query("SELECT m FROM Mob m WHERE m.mobNeededAccuracy <= :limit ORDER BY m.mobNeededAccuracy ASC")
-	public List<Mob> getAccuracyAscLimit(int limit);
-	
-	@Query("SELECT m FROM Mob m WHERE m.mobNeededAccuracy <= :limit ORDER BY m.mobNeededAccuracy Desc")
-	public List<Mob> getAccuracyDescLimit(int limit);
-	
-	@Query("SELECT m FROM Mob m ORDER BY m.mobMaxMeso ASC")
-	public List<Mob> getMaxMesoAsc();
-	
-	@Query("SELECT m FROM Mob m ORDER BY m.mobMaxMeso DESC")
-	public List<Mob> getMaxMesoDesc();
-	
-	@Query("SELECT m FROM Mob m ORDER BY m.mobMinMeso ASC")
-	public List<Mob> getMinMesoAsc();
-	
-	@Query("SELECT m FROM Mob m ORDER BY m.mobMinMeso DESC")
-	public List<Mob> getMinMesodesc();
 }
--- Artale_Database/src/main/java/com/artaleDB/services/MobService.java
+++ Artale_Database/src/main/java/com/artaleDB/services/MobService.java
@@ -18,55 +18,5 @@ public class MobService {
 	}
 	
 	
-	public List<Mob> getAll() {
-		return mobRepo.findAll();
-	}
 	
-	public Optional<Mob> returnById(long id) {
-		return mobRepo.findById(id);
-	}
-	
-	public List<Mob> returnListByName(String name) {
-		return mobRepo.getByName(name);
-	}
-	
-	public Mob returnByExactName(String name) {
-		return mobRepo.getByExactName(name);
-	}
-	
-	public List<Mob> returnListByLocation(String location) {
-		return mobRepo.getByLocations(location);
-	}
-	
-	public List<Mob> returnListByAccuracyAsc() {
-		return mobRepo.getByAccuracyAsc();
-	}
-	
-	public List<Mob> returnListByAccuracyDesc() {
-		return mobRepo.getByAccuracyDesc();
-	}
-	
-	public List<Mob> returnListByAccuracyAscLimit(int limit) {
-		return mobRepo.getAccuracyAscLimit(limit);
-	}
-	
-	public List<Mob> returnListByAccuracyDescLimit(int limit) {
-		return mobRepo.getAccuracyDescLimit(limit);
-	}
-	
-	public List<Mob> returnListByMaxMesoAsc() {
-		return mobRepo.getMaxMesoAsc();
-	}
-	
-	public List<Mob> returnListByMaxMesoDesc() {
-		return mobRepo.getMaxMesoDesc();
-	}
-	
-	public List<Mob> returnListByMinMesoAsc() {
-		return mobRepo.getMinMesoAsc();
-	}
-	
-	public List<Mob> returnListByMinMesoDesc() {
-		return mobRepo.getMinMesodesc();
-	}
 }
