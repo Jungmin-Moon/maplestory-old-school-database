a9fbc4296cdfce2f76bf5397953e2e50a8a66e2d
Jungmin Moon <jmmoon96@gmail.com>
2025-03-23 14:34:37 -0400

    Deleted old Boss to make it similar to Mob

--- Artale_Database/src/main/java/com/artaleDB/repositories/BossRepository.java
+++ Artale_Database/src/main/java/com/artaleDB/repositories/BossRepository.java
@@ -12,40 +12,5 @@ import com.artaleDB.entities.Boss;
 public interface BossRepository extends JpaRepository<Boss, Long>{
 	
 	
-	@Query("SELECT b FROM Boss b WHERE b.bossName LIKE %:name%")
-	public List<Boss> getByName(String name);
 	
-	
-	@Query("SELECT b FROM Boss b WHERE b.bossLocation LIKE %:location%")
-	public List<Boss> getByLocations(String location);
-	
-	@Query("SELECT b FROM Boss b ORDER BY b.bossLevel ASC")
-	public List<Boss> getByLevelAsc();
-	
-	@Query("SELECT b FROM Boss b ORDER BY b.bossLevel DESC")
-	public List<Boss> getByLevelDesc();
-	
-	@Query("SELECT b FROM Boss b ORDER BY b.bossHP ASC")
-	public List<Boss> getByBossHPAsc();
-	
-	@Query("SELECT b FROM Boss b ORDER BY b.bossHP DESC")
-	public List<Boss> getByBossHPDesc();
-	
-	@Query("SELECT b FROM Boss b ORDER BY b.bossMaxRespawn ASC")
-	public List<Boss> getByBossMaxRespawnAsc();
-	
-	@Query("SELECT b FROM Boss b ORDER BY b.bossMinRespawn ASC")
-	public List<Boss> getByBossMinRespawnAsc();
-	
-	@Query("SELECT b FROM Boss b ORDER BY b.bossMaxRespawn DESC")
-	public List<Boss> getByBossMaxRespawnDesc();
-	
-	@Query("SELECT b FROM Boss b ORDER BY b.bossMinRespawn DESC")
-	public List<Boss> getByBossMinRespawnDesc();
-	
-	@Query("SELECT b FROM Boss b WHERE b.bossMaxRespawn >= :time ORDER BY b.bossMaxRespawn DESC")
-	public List<Boss> getBossMaxRespawnLimit(int time);
-	
-	@Query("SELECT b FROM Boss b WHERE b.bossMinRespawn >= :time ORDER BY b.bossMinRespawn DESC")
-	public List<Boss> getBossMinRespawnLimit(int time);
 }
--- Artale_Database/src/main/java/com/artaleDB/services/BossService.java
+++ Artale_Database/src/main/java/com/artaleDB/services/BossService.java
@@ -17,59 +17,5 @@ public class BossService {
 		this.bossRepo = bossRepo;	
 	}
 	
-	public List<Boss> getAll() {
-		return bossRepo.findAll();
-	}
 	
-	public Optional<Boss> returnById(long id) {
-		return bossRepo.findById(id);
-	}
-	
-	public List<Boss> returnBossByName(String name) {
-		return bossRepo.getByName(name);
-	}
-	
-	public List<Boss> returnBossByLocation(String location) {
-		return bossRepo.getByLocations(location);
-	}
-	
-	public List<Boss> returnBossByHPAsc() {
-		return bossRepo.getByBossHPAsc();
-	}
-	
-	public List<Boss> returnBossByHPDesc() {
-		return bossRepo.getByBossHPDesc();
-	}
-	
-	public List<Boss> returnBossByLevelAsc() {
-		return bossRepo.getByLevelAsc();
-	}
-	
-	public List<Boss> returnBossByLevelDesc() {
-		return bossRepo.getByLevelDesc();
-	}
-	
-	public List<Boss> returnBossByMaxRespawnAsc() {
-		return bossRepo.getByBossMaxRespawnAsc();
-	}
-	
-	public List<Boss> returnBossByMaxRespawnDesc() {
-		return bossRepo.getByBossMaxRespawnDesc();
-	}
-	
-	public List<Boss> returnBossByMinRespawnAsc() {
-		return bossRepo.getByBossMinRespawnAsc();
-	}
-	
-	public List<Boss> returnBossByMinRespawnDesc() {
-		return bossRepo.getByBossMinRespawnDesc();
-	}
-	
-	public List<Boss> returnBossByMaxRespawnLimit(int time) {
-		return bossRepo.getBossMaxRespawnLimit(time);
-	}
-	
-	public List<Boss> returnBossByMinRespawnLimit(int time) {
-		return bossRepo.getBossMinRespawnLimit(time);
-	}
 }	
