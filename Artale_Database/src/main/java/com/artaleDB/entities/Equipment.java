7e36766620b79b5d1fb38de8b2f7898d96fa62b9
Jungmin Moon <jmmoon96@gmail.com>
2025-04-05 04:13:43 -0400

    Removed unused import

--- Artale_Database/src/main/java/com/artaleDB/entities/Equipment.java
+++ Artale_Database/src/main/java/com/artaleDB/entities/Equipment.java
@@ -1,7 +1,5 @@
 package com.artaleDB.entities;
 
-import java.util.List;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
