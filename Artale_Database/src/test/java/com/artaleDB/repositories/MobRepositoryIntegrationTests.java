87bd3e3fa2d9bb4a9ebb8c43b2893d415a426c36
Jungmin Moon <jmmoon96@gmail.com>
2025-04-08 20:09:32 -0400

    figured out the issue now to fix the test

--- Artale_Database/src/test/java/com/artaleDB/repositories/MobRepositoryIntegrationTests.java
+++ Artale_Database/src/test/java/com/artaleDB/repositories/MobRepositoryIntegrationTests.java
@@ -4,12 +4,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
 import com.artaleDB.entities.Mob;
 
-@DataJpaTest
+@DataJpaTest(showSql = true)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class MobRepositoryIntegrationTests {
 	
 	@Autowired
@@ -23,11 +25,22 @@ public class MobRepositoryIntegrationTests {
 	void givenNewMobWhenSaveThenSuccess() {
 		Mob newMob = new Mob();
 		
-		newMob.setId(203);
+		//newMob.setId(203);
 		newMob.setMobName("TEST");
+		newMob.setMobEXP(0);
+		newMob.setMobHP(0);
+		newMob.setMobLevel(0);
+		newMob.setMobLocation("tesLocation");
+		newMob.setMobLocationTwo("test_location two");
+		newMob.setMobMaxMeso(0);
+		newMob.setMobMinMeso(0);
+		newMob.setMobMP(0);
+		newMob.setMobNeededAccuracy(0);
 		
-		Mob insertedMob = mobRepository.save(newMob);
+		mobRepository.save(newMob);
 		
-		assertThat(entityManager.find(Mob.class, insertedMob.getId())).isEqualTo(newMob);
+		//assertThat(entityManager.find(Mob.class, insertedMob.getId())).isEqualTo(newMob);
+		
+		assertThat(mobRepository.findById(newMob.getId())).isEqualTo(newMob.getId());
 	}
 }
