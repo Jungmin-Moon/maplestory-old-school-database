880cba2ae36542467e79109e6aeb728fffbd998c
Jungmin Moon <jmmoon96@gmail.com>
2025-04-11 19:22:19 -0400

    BossControllerAdvice complete with exception handling

--- Artale_Database/src/main/java/com/artaleDB/controllers/BossControllerAdvice.java
+++ Artale_Database/src/main/java/com/artaleDB/controllers/BossControllerAdvice.java
@@ -1,8 +1,25 @@
 package com.artaleDB.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.artaleDB.entities.NoMatchingLocationException;
+import com.artaleDB.entities.NoneFoundException;
 
 @RestControllerAdvice
 public class BossControllerAdvice {
-
+	
+	@ExceptionHandler(NoneFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	String noBossesFoundHanlder(NoneFoundException ex) {
+		return ex.getMessage();
+	}
+	
+	@ExceptionHandler(NoMatchingLocationException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	String noMatchingLocationhandler(NoMatchingLocationException ex) {
+		return ex.getMessage();
+	}
 }
