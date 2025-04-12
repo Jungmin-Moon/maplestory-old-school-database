139c66717d09e6afee4436edec42a8901dbe2ff6
Jungmin Moon <jmmoon96@gmail.com>
2025-04-11 20:24:03 -0400

    EquipmentControllerAdvice is fine for now

--- Artale_Database/src/main/java/com/artaleDB/controllers/EquipmentControllerAdvice.java
+++ Artale_Database/src/main/java/com/artaleDB/controllers/EquipmentControllerAdvice.java
@@ -1,8 +1,19 @@
 package com.artaleDB.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.artaleDB.entities.NoneFoundException;
 
 @RestControllerAdvice
 public class EquipmentControllerAdvice {
-
+	
+	@ExceptionHandler(NoneFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	String noEquipmentFoundHandler(NoneFoundException ex) {
+		return ex.getMessage();
+	}
+	
 }
