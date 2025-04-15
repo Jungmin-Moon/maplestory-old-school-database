521b3c8525adb8dc10ee617701685faa4d9561ca
Jungmin Moon <jmmoon96@gmail.com>
2025-04-15 02:49:15 -0400

    Initial drop table design for mobs and bosses

---
+++ Artale_Database/src/main/resources/db/migration/V1__boss_drop_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `artale_boss_droplist` (
+  `boss_id` int NOT NULL,
+  `equipment_id` int NOT NULL
+) 
+
+/* Most likely not the best way but the idea is to map the boss_id with an equipment_id drop and repeat boss_id multiple times */
---
+++ Artale_Database/src/main/resources/db/migration/V1__mob_drop_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `artale_mob_droplist` (
+  `mob_id` int NOT NULL,
+  `equipment_id` int NOT NULL
+) 
+
+
+/* Most likely not the best way but the idea is to map the mob_id with an equipment_id drop and repeat boss_id multiple times */
