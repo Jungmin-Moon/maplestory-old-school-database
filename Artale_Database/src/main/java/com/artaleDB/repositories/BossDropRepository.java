f413c999676e370da4c05e90682b29bf7666b768
Jungmin Moon <jmmoon96@gmail.com>
2025-06-17 11:56:34 -0400

    Repositories for the drops set up

---
+++ Artale_Database/src/main/java/com/artaleDB/repositories/BossDropRepository.java
@@ -0,0 +1,9 @@
+package com.artaleDB.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.artaleDB.entities.BossDrops;
+
+public interface BossDropRepository extends JpaRepository<BossDrops, Long>{
+
+}
---
+++ Artale_Database/src/main/java/com/artaleDB/repositories/MobDropRepository.java
@@ -0,0 +1,9 @@
+package com.artaleDB.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.artaleDB.entities.MobDrops;
+
+public interface MobDropRepository extends JpaRepository<MobDrops, Long>{
+
+}
