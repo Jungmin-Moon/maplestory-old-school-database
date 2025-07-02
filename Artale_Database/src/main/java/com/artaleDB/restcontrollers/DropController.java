b6cc390ae94b2fdbfb23704e9e3d438260334cc4
Jungmin Moon <jmmoon96@gmail.com>
2025-07-02 16:10:54 -0400

    More small work on the rest controller for drops

--- Artale_Database/src/main/java/com/artaleDB/restcontrollers/DropController.java
+++ Artale_Database/src/main/java/com/artaleDB/restcontrollers/DropController.java
@@ -1,8 +1,13 @@
 package com.artaleDB.restcontrollers;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.artaleDB.entities.BossDrops;
+import com.artaleDB.entities.MobDrops;
 import com.artaleDB.services.DropService;
 
 @RestController
@@ -15,6 +20,20 @@ public class DropController {
 		this.dropService = dropService;
 	}
 	
+	//Mob Drops
+	
+	@GetMapping("/mobs")
+	public List<MobDrops> getAllMobs() {
+		return dropService.findAllMobDrops();
+	}
+	
+	
+	//Boss Drops
+	
+	@GetMapping("/boss")
+	public List<BossDrops> getAllBoss() {
+		return dropService.findAllBossDrops();
+	}
 	
 	
 }
