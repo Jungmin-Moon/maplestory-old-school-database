80d348df3fcd3b36ec4ccd37b89137604125ef15
Jungmin Moon <jmmoon96@gmail.com>
2025-07-03 03:59:44 -0400

    Need to separate boss and mob drop search i feel since this isn't a
    react or angular front end

---
+++ Artale_Database/src/main/java/com/artaleDB/web/DropWebController.java
@@ -0,0 +1,10 @@
+package com.artaleDB.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/web/drop")
+public class DropWebController {
+
+}
