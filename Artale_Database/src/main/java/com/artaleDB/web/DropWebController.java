278e32dc2c57c3924a3ea1cf3a1ce092a25a288c
Jungmin Moon <jmmoon96@gmail.com>
2025-07-04 04:27:55 -0400

    Need to work on HTML layout for drop pages

--- Artale_Database/src/main/java/com/artaleDB/web/DropWebController.java
+++ Artale_Database/src/main/java/com/artaleDB/web/DropWebController.java
@@ -1,10 +1,43 @@
 package com.artaleDB.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.artaleDB.services.DropService;
 
 @Controller
 @RequestMapping("/web/drop")
 public class DropWebController {
 
+	DropService dropService;
+	
+	DropWebController(DropService dropService) {
+		this.dropService = dropService;
+	}
+	
+	
+	@GetMapping("/mob")
+	public String mobDropHome(Model model) {
+		
+		var allMobDrops = dropService.findAllMobDropsWeb();
+		
+		model.addAttribute("mobDropsAll", allMobDrops);
+		
+		return "mob_drops";
+	}
+	
+	
+	@GetMapping("/boss") 
+	public String bossDropHome(Model model) {
+		
+		var allBossDrops = dropService.findAllBossDropsWeb();
+		
+		model.addAttribute("bossDropsAll", allBossDrops);
+		
+		return "boss_drops";
+	}
+	
+	
 }
