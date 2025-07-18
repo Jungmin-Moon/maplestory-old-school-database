22adde966b35bd2fc39802606d764430c79a0a7d
Jungmin Moon <jmmoon96@gmail.com>
2025-07-18 03:41:17 -0400

    Need to adjust checkboxes to not send null

--- Artale_Database/src/main/java/com/artaleDB/web/EquipmentWebController.java
+++ Artale_Database/src/main/java/com/artaleDB/web/EquipmentWebController.java
@@ -3,9 +3,11 @@ package com.artaleDB.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.artaleDB.dto.UserSearchQueryEquipment;
 import com.artaleDB.services.EquipmentService;
 
 @Controller
@@ -46,6 +48,35 @@ public class EquipmentWebController {
 		var allEquipment = equipmentService.findAllWeb();
 		
 		model.addAttribute("allEquipment", allEquipment);
+		
+		return "equipment";
+	}
+	
+	@PostMapping
+	public String afterSearchEquipment(Model model, @RequestParam (required = false) String home, @RequestParam (required = false) String mob,
+							@RequestParam (required = false) String boss, @RequestParam (required = false) String mobdrop, @RequestParam (required = false) String bossdrop, 
+							UserSearchQueryEquipment uSearchEquipment) {
+		if (home != null) {
+			return "redirect:/home";
+		}
+		
+		if (mob != null) {
+			return "redirect:/web/mob";
+		}
+		
+		if (boss != null) {
+			return "redirect:/web/boss";
+		}
+		
+		if (mobdrop != null) {
+			return "redirect:/web/drop/mob";
+		}
+		
+		if (bossdrop != null) {
+			return "redirect:/web/drop/boss";
+		}
+		
+		System.out.println(uSearchEquipment.isArcher());
 		
 		return "equipment";
 	}
