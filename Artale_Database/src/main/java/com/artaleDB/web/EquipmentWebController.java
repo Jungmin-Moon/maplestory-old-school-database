f51988c6433ffd58a6efe327b677abc55c53f104
Jungmin Moon <jmmoon96@gmail.com>
2025-10-04 02:37:46 -0400

    First steps work getting a list of equipment is possible to parse
    with a List now to mess with hyperlinks

--- Artale_Database/src/main/java/com/artaleDB/web/EquipmentWebController.java
+++ Artale_Database/src/main/java/com/artaleDB/web/EquipmentWebController.java
@@ -1,5 +1,6 @@
 package com.artaleDB.web;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -24,9 +25,9 @@ public class EquipmentWebController {
 	@GetMapping
 	public String equipment(Model model, @RequestParam (required = false) String home, @RequestParam (required = false) String mob,
 							@RequestParam (required = false) String boss, @RequestParam (required = false) String mobdrop, @RequestParam (required = false) String bossdrop,
-							@RequestParam (required = false) List<String> equipmentType, @RequestParam(required = false) int minimumLevel, @RequestParam(required = false) int warrior,
-							@RequestParam(required = false) int magician, @RequestParam(required = false) int thief, @RequestParam(required = false) int archer, 
-							@RequestParam(required = false) int pirate, @RequestParam(required = false) int beginner, @RequestParam(required = false) int common) {
+							@RequestParam (required = false) List<String> equipmentType, @RequestParam(required = false) Integer minimumLevel, @RequestParam(required = false) Integer warrior,
+							@RequestParam(required = false) Integer magician, @RequestParam(required = false) Integer thief, @RequestParam(required = false) Integer archer, 
+							@RequestParam(required = false) Integer pirate, @RequestParam(required = false) Integer beginner, @RequestParam(required = false) Integer common) {
 		
 		if (home != null) {
 			return "redirect:/home";
@@ -48,9 +49,17 @@ public class EquipmentWebController {
 			return "redirect:/web/drop/boss";
 		}
 		
-		var allEquipment = equipmentService.findAllWeb();
+		if (equipmentType == null ||minimumLevel == null || warrior == null || magician == null || thief == null || archer == null || pirate == null || beginner == null || common == null) {
+			minimumLevel = warrior = magician = thief = archer = pirate = beginner = common = 0;
+			equipmentType = new ArrayList<>();
+		}
 		
-		model.addAttribute("allEquipment", allEquipment);
+		
+		/*
+		System.out.println("Chosen equipment are: ");
+		for(String s: equipmentType) {
+			System.out.println(s);
+		} */
 		
 		return "equipment";
 	}
