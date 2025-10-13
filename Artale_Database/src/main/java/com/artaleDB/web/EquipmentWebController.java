a5926b54deac6ac30a0bd3cfc9991132633422d4
Jungmin Moon <jmmoon96@gmail.com>
2025-10-13 05:38:49 -0400

    Equipment Web Controller now produces Kafka messages

--- Artale_Database/src/main/java/com/artaleDB/web/EquipmentWebController.java
+++ Artale_Database/src/main/java/com/artaleDB/web/EquipmentWebController.java
@@ -1,5 +1,7 @@
 package com.artaleDB.web;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.artaleDB.dto.UserSearchQueryEquipment;
 import com.artaleDB.entities.Equipment;
+import com.artaleDB.kafka.KafkaSender;
 import com.artaleDB.services.EquipmentService;
 
 @Controller
@@ -23,11 +26,12 @@ import com.artaleDB.services.EquipmentService;
 public class EquipmentWebController {
 	
 	EquipmentService equipmentService;
+	KafkaSender kafkaSender;
 	
-	EquipmentWebController(EquipmentService equipmentService) {
+	EquipmentWebController(EquipmentService equipmentService, KafkaSender kafkaSender) {
 		this.equipmentService = equipmentService;
+		this.kafkaSender = kafkaSender;
 	}
-	
 	
 	@GetMapping
 	public String equipment(Model model, @RequestParam (required = false) String home, @RequestParam (required = false) String mob,
@@ -40,23 +44,31 @@ public class EquipmentWebController {
 							@RequestParam("magician") Optional<Integer> mag, @RequestParam("thief") Optional<Integer> thf, @RequestParam("archer") Optional<Integer> arc, @RequestParam("pirate") Optional<Integer> pir, 
 							@RequestParam("common") Optional<Integer> comm, @RequestParam("beginner") Optional<Integer> beg) {
 		
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy hh:mm");
+		
+		
 		if (home != null) {
+			kafkaSender.sendMessageLinkClick("Link-Click-Events", LocalDateTime.now().format(formatter).toString(), "Equipment search to Home was clicked.");
 			return "redirect:/home";
 		}
 		
 		if (mob != null) {
+			kafkaSender.sendMessageLinkClick("Link-Click-Events", LocalDateTime.now().format(formatter).toString(), "Equipment search to Mob Search was clicked.");
 			return "redirect:/web/mob";
 		}
 		
 		if (boss != null) {
+			kafkaSender.sendMessageLinkClick("Link-Click-Events", LocalDateTime.now().format(formatter).toString(), "Equipment search to Boss Search was clicked.");
 			return "redirect:/web/boss";
 		}
 		
 		if (mobdrop != null) {
+			kafkaSender.sendMessageLinkClick("Link-Click-Events", LocalDateTime.now().format(formatter).toString(), "Equipment search to Mob Drop Search was clicked.");
 			return "redirect:/web/drop/mob";
 		}
 		
 		if (bossdrop != null) {
+			kafkaSender.sendMessageLinkClick("Link-Click-Events", LocalDateTime.now().format(formatter).toString(), "Equipment search to Boss Drop Search was clicked.");
 			return "redirect:/web/drop/boss";
 		}
 		
@@ -82,9 +94,12 @@ public class EquipmentWebController {
 			equipmentType = new ArrayList<>();
 			
 			equipPage = equipmentService.findAllWeb(PageRequest.of(currentPage - 1, pageSize));
+			
 		} else {
 			
 			UserSearchQueryEquipment uSearchEquip = new UserSearchQueryEquipment(eName, eType, minLevel, classWar, classMagician, classThief, classArcher, classPirate, classBeginner, classCommon);
+			
+			kafkaSender.sendMessageEquipmentSearch("Equipment-Search-Choices", LocalDateTime.now().format(formatter).toString(), uSearchEquip);
 			
 			equipPage = equipmentService.findAllByQuery(PageRequest.of(currentPage - 1, pageSize), uSearchEquip);	
 			
