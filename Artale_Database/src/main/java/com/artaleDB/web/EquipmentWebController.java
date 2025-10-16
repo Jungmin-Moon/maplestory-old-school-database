0908e369c9fe577f361d87a5db692df78be85120
Jungmin Moon <jmmoon96@gmail.com>
2025-10-16 04:57:55 -0400

    Resolved issue with Equipment search. Need to work on drop tables now.

--- Artale_Database/src/main/java/com/artaleDB/web/EquipmentWebController.java
+++ Artale_Database/src/main/java/com/artaleDB/web/EquipmentWebController.java
@@ -39,10 +39,7 @@ public class EquipmentWebController {
 							@RequestParam(required = false) String equipmentName, @RequestParam (required = false) List<String> equipmentType, @RequestParam(required = false) Integer minimumLevel, 
 							@RequestParam(required = false) Integer warrior, @RequestParam(required = false) Integer magician, @RequestParam(required = false) Integer thief, 
 							@RequestParam(required = false) Integer archer, @RequestParam(required = false) Integer pirate, @RequestParam(required = false) Integer beginner, 
-							@RequestParam(required = false) Integer common, @RequestParam("page") Optional<Integer> page, @RequestParam("size") Optional<Integer> size, @RequestParam("equipmentName") Optional<String> equipName,
-							@RequestParam("equipmentType") Optional<List<String>> equipType, @RequestParam("minimumLevel") Optional<Integer> level, @RequestParam("warrior") Optional<Integer> war, 
-							@RequestParam("magician") Optional<Integer> mag, @RequestParam("thief") Optional<Integer> thf, @RequestParam("archer") Optional<Integer> arc, @RequestParam("pirate") Optional<Integer> pir, 
-							@RequestParam("common") Optional<Integer> comm, @RequestParam("beginner") Optional<Integer> beg) {
+							@RequestParam(required = false) Integer common, @RequestParam("page") Optional<Integer> page, @RequestParam("size") Optional<Integer> size) {
 		
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy hh:mm");
 		
@@ -73,55 +70,68 @@ public class EquipmentWebController {
 		}
 		
 		int currentPage = page.orElse(1);
-		int pageSize = size.orElse(20);
+		int pageSize = size.orElse(25);
 		
-		//Used for URL creation and to make sure queries are kept intact
-		String eName = equipName.orElse("");
-		List<String> eType = equipType.orElse(equipmentType);
-		int minLevel = level.orElse(0);
-		int classWar = war.orElse(0);
-		int classMagician = mag.orElse(0);
-		int classThief = thf.orElse(0);
-		int classArcher = arc.orElse(0);
-		int classPirate = pir.orElse(0);
-		int classCommon = comm.orElse(0);
-		int classBeginner = beg.orElse(0);
+		String eName = "";
+		List<String> eType = new ArrayList<>();
+		int eLevel = 0;
+		int eWarrior = 0;
+		int eMagician = 0;
+		int eThief = 0;
+		int eArcher = 0;
+		int ePirate = 0;
+		int eCommon = 0;
+		int eBeginner = 0;
 		
 		Page<Equipment> equipPage;
 		
-		
-		if (equipmentName == null || equipmentType == null ||minimumLevel == null || warrior == null || magician == null || thief == null || archer == null || pirate == null || beginner == null || common == null) {
-			equipmentName = "";
-			minimumLevel = warrior = magician = thief = archer = pirate = beginner = common = 0;
-			equipmentType = new ArrayList<>();
+		if (equipmentName == null && equipmentType == null && minimumLevel == null && warrior == null && magician == null && thief == null && archer == null && pirate == null && common == null && beginner == null) {
 			
 			equipPage = equipmentService.findAllWeb(PageRequest.of(currentPage - 1, pageSize));
-			
 		} else {
 			
-			UserSearchQueryEquipment uSearchEquip = new UserSearchQueryEquipment(eName, eType, minLevel, classWar, classMagician, classThief, classArcher, classPirate, classBeginner, classCommon);
+			if (equipmentType == null) {
+				eType = new ArrayList<>();
+			} else {
+				eType = equipmentType;
+			}
 			
-			kafkaSender.sendMessageEquipmentSearch("Equipment-Search-Choices", LocalDateTime.now().format(formatter).toString(), uSearchEquip);
+			eName = equipmentName;
+			eLevel = minimumLevel;
+			eWarrior = warrior;
+			eMagician = magician;
+			eThief = thief;
+			eArcher = archer;
+			ePirate = pirate;
+			eCommon = common;
+			eBeginner = beginner;
 			
-			equipPage = equipmentService.findAllByQuery(PageRequest.of(currentPage - 1, pageSize), uSearchEquip);	
+			
+			UserSearchQueryEquipment uEquipSearch = new UserSearchQueryEquipment(eName, eType, eLevel, eWarrior, eMagician, eThief, eArcher, ePirate, eBeginner, eCommon);
+			kafkaSender.sendMessageEquipmentSearch("Equipment-Search-Choices", LocalDateTime.now().format(formatter).toString(), uEquipSearch);
+			
+			equipPage = equipmentService.findAllByQuery(PageRequest.of(currentPage - 1, pageSize), uEquipSearch);
+			
+			
 			
 		}
 		
-		int totalPages = equipPage.getTotalPages();
-		
-		
 		model.addAttribute("equipPage", equipPage);
 		model.addAttribute("currentPage", currentPage);
-		model.addAttribute("equipName", eName);
-		model.addAttribute("equipmentTypeQuery", eType);
-		model.addAttribute("level", minLevel);
-		model.addAttribute("warrior", classWar);
-		model.addAttribute("magician", classMagician);
-		model.addAttribute("thief", classThief);
-		model.addAttribute("archer", classArcher);
-		model.addAttribute("pirate", classPirate);
-		model.addAttribute("common", classCommon);
-		model.addAttribute("beginner", classBeginner);
+		
+		//Filters
+		model.addAttribute("equipNameFilter", eName);
+		model.addAttribute("equipmentTypeFilter", eType);
+		model.addAttribute("levelFilter", eLevel);
+		model.addAttribute("warriorFilter", eWarrior);
+		model.addAttribute("magicianFilter", eMagician);
+		model.addAttribute("thiefFilter", eThief);
+		model.addAttribute("archerFilter", eArcher);
+		model.addAttribute("pirateFilter", ePirate);
+		model.addAttribute("commonFilter", eCommon);
+		model.addAttribute("beginnerFilter", eBeginner);
+		
+		int totalPages = equipPage.getTotalPages();
 		
 		if (totalPages > 0) {
 			List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
@@ -129,6 +139,7 @@ public class EquipmentWebController {
 												.collect(Collectors.toList());
 			model.addAttribute("pageNumbers", pageNumbers);
 		}
+		
 		
 		
 		return "equipment";
